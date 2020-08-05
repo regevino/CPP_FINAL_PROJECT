c5e1109332f5a8ec6f89b50995e0ff9678390d2d
Dan Regev <dan.regev@mail.huji.ac.il>
2020-08-05 16:26:06 +0300

    Implemented about 50% of class functions, excluding iterator

--- VLVector.hpp
+++ VLVector.hpp
@@ -16,7 +16,8 @@ class VLVector
 private:
     bool stackMode;
     std::size_t _size;
-    std::unique_ptr<T> heapVec;
+    std::size_t _capacity;
+    std::unique_ptr<T[]> heapVec;
     T stackVec[StaticCapacity];
 
     /**
@@ -111,18 +112,40 @@ private:
         typedef std::random_access_iterator_tag iterator_category;
     private:
     };
+
+    /**
+     * @brief Copies the elements of the vector from the stack to the heap.
+     * Sets the flag stackMode to false.
+     */
+    void copyToHeap()
+    {
+        stackMode = false;
+        heapVec = std::make_unique<T[]>(_capacity);
+        for (int i = 0; i < (int) StaticCapacity; ++i)
+        {
+            heapVec[i] = stackVec[i];
+        }
+    }
+
+    /**
+     * @brief Copies the elements of the vector from the heap to the stack.
+     * Sets the flag stackMode to true.
+     */
+    void copyToStack()
+    {
+        stackMode = true;
+        for (int i = 0; i < (int) _size; ++i)
+        {
+            stackVec[i] = heapVec[i];
+        }
+        heapVec.reset();
+    }
 public:
     /**
      * @brief Default constructor. Initialises an empty VLVector.
      */
-    VLVector() : stackMode(true), _size(0)
-    //, heapVec(new T[0], [&](T *p){delete[] p;})
-    {
-        heapVec = std::make_unique<T>();
-        heapVec.get()[0] = 1;
-        std::cerr << heapVec.get()[0]<<'\n';//TODO CHECK IF MEMORY WAS RELEASED PROPERLY
-    }
-    /** stackVec()*///TODO IS THIS NEEDED?
+    VLVector() : stackMode(true), _size(0), _capacity(StaticCapacity), heapVec(nullptr){} //, [&](T *p){delete[]
+    // p;}
 
     /**
      * @brief Constructs a VLVector object.
@@ -131,33 +154,39 @@ public:
      * @param first iterator to the first T value in the group.
      * @param last iterator to the last T value in the group.
      */
-    template<class InputIterator>
-    VLVector(InputIterator& first, InputIterator& last){}
+    template<class InputIterator> //TODO
+    VLVector(InputIterator& first, InputIterator& last) : stackMode(true), _size(0), _capacity(StaticCapacity)
+    {
+//        for (auto &it = first; it != last; ++it)
+//        {
+//            insert();
+//        }
+    }
 
     /**
      * @brief Returns the number of elements that are stored in the vector.
      * @return the number of elements that are stored in the vector.
      */
-    inline std::size_t size() const {return this->_size;}
+    std::size_t size() const {return _size;}
 
     /**
-     * @brief Returns the capacity of the vector.
+     * @brief Returns the capacity of the vector according to the formula given in the exam.
      * @return the capacity of the vector.
      */
     std::size_t capacity() const
     {
-        if ((int)this->_size + 1 <= StaticCapacity)
+        if ((int)_size + 1 <= StaticCapacity)
         {
             return StaticCapacity;
         }
-        return (size_t)(3 * (this->_size + 1) / 2);
+        return (size_t)(3 * (_size + 1) / 2);
     }
 
     /**
      * @brief Checks if the vector is empty.
      * @return true iff the vector is empty.
      */
-    inline bool empty() const {return _size == 0;}
+    bool empty() const {return _size == 0;}
 
     /**
      * @brief Gets an index and returns a reference to the value associated to it.
@@ -167,9 +196,13 @@ public:
      */
     T &at(const int index)
     {
-        if (index >= 0 && index <= (int)this->_size)
+        if (index >= 0 && index <= (int)_size)
         {
-
+            if (stackMode)
+            {
+                return stackVec[index];
+            }
+            return heapVec[index];
         }
         throw std::out_of_range(AT_EXCEPTION_MSG);
     }
@@ -180,19 +213,81 @@ public:
      * @param index the index of the value in the vector.
      * @return the value that is associated to the index.
      */
-    T at(const int index) const {}
+    T at(const int index) const
+    {
+        if (index >= 0 && index <= (int)_size)
+        {
+            if (stackMode)
+            {
+                return stackVec[index];
+            }
+            return heapVec[index];
+        }
+        throw std::out_of_range(AT_EXCEPTION_MSG);
+    }
 
     /**
      * @brief Adds a given value to the end of the vector.
      * @param val the value to add, given as an l-value.
      */
-    void push_back(const T &val){}
+    void push_back(const T &val)
+    {
+        std::size_t newCapacity = capacity();
+        if (stackMode) //We are in stack mode - values are stored on the stack:
+        {
+            //If the capacity that was calculated before does not exceed the static capacity:
+            if (newCapacity <= StaticCapacity)
+            {
+                stackVec[_size] = val;
+            }
+            else //If the capacity that was calculated before exceeds the static capacity:
+            {
+                copyToHeap();
+                heapVec[_size] = val;
+            }
+        }
+        else //We are in heap mode - values are stored on the heap:
+        {
+            if (_size + 1 > _capacity)
+            {
+                auto newHeap = std::make_unique<T[]>(newCapacity);
+                for (int i = 0; i < _size; ++i)
+                {
+                    newHeap[i] = heapVec[i];
+                }
+                _capacity = newCapacity;
+            }
+            heapVec[_size] = val;
+        }
+        ++_size;
+    }
 
     /**
      * @brief Adds a given value to the end of the vector.
      * @param val the value to add, given as an r-value.
      */
-    void push_back(const T &&val){}
+    void push_back(const T &&val)
+    {
+        _capacity = capacity();
+        if (stackMode) //We are in stack mode - values are stored on the stack:
+        {
+            //If the capacity that was calculated before does not exceed the static capacity:
+            if (_capacity <= StaticCapacity)
+            {
+                stackVec[_size] = val;
+            }
+            else //If the capacity that was calculated before exceeds the static capacity:
+            {
+                copyToHeap();
+                heapVec[_size] = val;
+            }
+        }
+        else //We are in heap mode - values are stored on the heap:
+        {
+            heapVec[_size] = val;
+        }
+        ++_size;
+    }
 
     /**
      * @brief Adds a given value to the vector at the position before
@@ -200,7 +295,7 @@ public:
      * @param position the position to add the value before it.
      * @param val the value to add, given as an l-value.
      * @return an iterator that points to the added value.
-     */
+     */ //TODO
     iterator insert(const iterator &position, const T &val){}
 
     /**
@@ -209,24 +304,45 @@ public:
      * @param position the position to add the value before it.
      * @param val the value to add, given as an r-value.
      * @return an iterator that points to the added value.
-     */
+     */ //TODO
     iterator insert(const iterator &position, const T &&val){}
 
     /**
      * @brief Removes the last element from the vector.
-     */
-    void pop_back(){}
+     */ //TODO
+    void pop_back()
+    {
+        if (!stackMode) //We are in heap mode - values are stored on the heap:
+        {
+            //If the capacity that was calculated before does not exceed the static capacity:
+            if (_capacity <= StaticCapacity)
+            {
+            }
+            else //If the capacity that was calculated before exceeds the static capacity:
+            {
+                copyToHeap();
+            }
+        }
+        else //We are in stack mode - values are stored on the stack:
+        {
+            if (_size > 0)
+            {
+                stackVec[_size - 1] = T();
+            }
+        }
+        --_size;
+    }
 
     /**
      * @brief Removes from the vector the value that the given iterator points to.
      * @param position an iterator that points to the value that is to be removed.
      * @return an iterator to the value that appeared after the removed value.
-     */
+     */ //TODO
     iterator erase(iterator &position){}
 
     /**
      * @brief Removes all elements from the vector.
-     */
+     */ //TODO
     void clear(){}
 
     /**
@@ -237,9 +353,9 @@ public:
     {
         if (stackMode)
         {
-            return &this->stackVec;
+            return stackVec;
         }
-        return this->heapVec;
+        return heapVec.get();
     }
 
     /**
@@ -248,7 +364,14 @@ public:
      * @param index the given index.
      * @return a reference to the value associated to the index.
      */
-    T &operator[](const int index) noexcept{}
+    T &operator[](const int index) noexcept
+    {
+        if (index >= 0 && index < (int)_size)
+        {
+            return data()[index];
+        }
+        return T();
+    }
 
     /**
      * @brief Gets an index and returns the value associated to it.
@@ -256,26 +379,50 @@ public:
      * @param index the given index.
      * @return the value associated to the index.
      */
-    T operator[](const int index) const noexcept{}
+    const T &operator[](const int index) const noexcept
+    {
+        if (index >= 0 && index < (int)_size)
+        {
+            return data()[index];
+        }
+        return T();
+    }
 
     /**
      * @brief Checks if this object equals to another VLVector object.
      * @param other the other VLVector object.
      * @return true iff the objects are the same.
      */
-    bool operator==(const VLVector &other) const{}
+    bool operator==(const VLVector &other) const
+    {
+        //Check if this vector's size matches the other vector's size:
+        if (_size != other._size)
+        {
+            return false;
+        }
+
+        // Check that each element in this vector compares equal
+        // with the element in the other vector at the same position:
+        for (int i = 0; i < (int) _size; ++i)
+        {
+            if (*this[i] != other[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     /**
      * @brief Checks if this object differs from another VLVector object.
      * @param other the other VLVector object.
      * @return true iff the objects differ from each other.
      */
-    bool operator!=(const VLVector &other) const
-    {
-        return !operator==(other);
-    }
+    bool operator!=(const VLVector &other) const {return !operator==(other);}
 
-    inline iterator begin(){return iterator();}
+    //TODO
+    iterator begin(){return iterator();}
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "VLVector.hpp"
+#include <vector>
 int main()
 {
     VLVector<int> v;
 
+    auto a = std::make_unique<int[]>(2);
+    a[0] = 1;
+    a[1] = 2;
+    std::cerr << a[0] << '\n';
+    std::cerr << a[1] << '\n';
     return 0;
 }
